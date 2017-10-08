f83849a5501bea9394aa32cdb5b6fa7f08255128
Iftekhar Ahmed Khan <iftekharkhan09@users.noreply.github.com>
2017-10-08 14:27:04 +0530

    Initial Commit.

---
+++ src/main/java/com/spring/aouth/SpringAuthSecurity/SpringAuthSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.spring.aouth.SpringAuthSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthSecurityApplication.class, args);
+	}
+}
